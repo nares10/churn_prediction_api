e43bfccba13c0f70a6b592b8875ac46f38437ee0
Naresh Dewasi <nareshdewasi021@gmail.com>
2026-01-29 19:51:01 +0530

    Fixed mistake

--- app/schema.py
+++ app/schema.py
@@ -4,5 +4,9 @@ class CustomerInput(BaseModel):
     Tenure: int
     MonthlyCharges: float
     Contract: str
+<<<<<<< HEAD
     TotalCharges : float
+=======
+    TotalCharges: float
+>>>>>>> 7747779 (Fixed mistake)
     PaymentMethod: str
